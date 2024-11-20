09f15c589f511783f72e0f553b52afd36c133aae
LiG <101067974+salaheddine-h@users.noreply.github.com>
2024-11-20 21:54:02 +0100

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -12,60 +12,48 @@
 
 # include "ft_printf.h"
 
-// Function li ghadi t3ref type dial formatting
-/*int	check_format(va_list args, char *format)
+static void	ft_format(va_list args, const char format, int *len)
 {
-	int	length;
-	int	i;
+	if (format == 'c')
+		ft_putchar(va_arg(args, int), len);
+	else if (format == 's')
+		ft_putstr(va_arg(args, char *), len);
+	else if (format == 'd' || format == 'i')
+		ft_putnbr(va_arg(args, int), len);
+	else if (format == 'u')
+		ft_putunbr(va_arg(args, unsigned int), len);
+	else if (format == 'x')
+		ft_puthex(va_arg(args, int), "0123456789abcdef", len);
+	else if (format == 'X')
+		ft_puthex(va_arg(args, int), "0123456789ABCDEF", len);
+	else if (format == 'p')
+		ft_putadd(va_arg(args, void *), len);
+	else if (format == '%')
+		ft_putchar('%', len);
+}
 
-	length = 0;
-	i = 0;
-	if (format[i] == 'c') // %c Prints a single character
-		length += ft_putchar(va_arg(args, char));
-	else if (format[i] == 's') //%s Prints a string (as defined by the common C convention).
-		length += ft_putstr(va_arg(args, char *));
-	else if (format[i] == 'd' || format == 'i') //%i Prints an integer in base 10
-		length += ft_putnbr(va_arg(args, int));
-	else if (format[i] == 'p') // %p The void
-		length += ft_putnbr(va_arg(args, void *));
-	else if (format[i] == 'u') //
-		length += ft_putnbr(va_arg(args, int));
-	else if (format[i] == 'x')
-		length += ft_putnbr(va_arg(argc, hex));
-	return (length);
-}*/
-
-int	ft_printf(const char *str, ...) 
+int	ft_printf(const char *str, ...)
 {
-	va_list	args;
 	int		i;
-	int		count;
-	
-	count = 0;
+	int		len;
+	va_list	args;
+
+	i = 0;
+	len = 0;
 	if (!str)
 		return (-1);
 	va_start(args, str);
-	while (*str)
+	while (str[i])
 	{
-		if (*str == '%')
+		if (str[i] == '%')
 		{
-			str++;
-			if(*str == 's')
-			{
-				char	*str = va_arg(args, char *); 
-				while(*str)
-				{
-					write(1, str, 1);
-					str++;
-				}
-			}
+			i++;
+			ft_format(args, str[i], &len);
 		}
+		else
+			ft_putchar(str[i], &len);
+		i++;
 	}
 	va_end(args);
-	return (count);
-}
-
-int	main(void)
-{
-	ft_printf("%s", "");
+	return (len);
 }
